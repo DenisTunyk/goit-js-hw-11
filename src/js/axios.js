1fff0bb6c0a5cc8c55442f0674fdf8a4dd2e1e8c
DenisTunyk <tundenis43@gmail.com>
2022-12-05 23:16:57 +0200

    Update axios.js

--- src/js/axios.js
+++ src/js/axios.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
 const BASE_URL = `https://pixabay.com/api`;
-const API_KEY = `31824211-7ef4149e7674ea42a1856045np8`;
+const API_KEY = `31824211-7ef4149e7674ea42a18560458`;
 
 export const fetchPics = async (input, page = "1") => {
   const response = await axios.get(`${BASE_URL}?key=${API_KEY}&q=${input}&orientation=horizontal&safesearch=true&image_type=photo&per_page=40&page=${page}`)
   return response.data
 };
 
-//https://pixabay.com/api?key=31824211-7ef4149e7674ea42a1856045np8&q=cat&orientation=horizontal&safesearch=true&image_type=photo&per_page=40&page=1
+//https://pixabay.com/api?key=31824211-7ef4149e7674ea42a18560458&q=cat&orientation=horizontal&safesearch=true&image_type=photo&per_page=40&page=1
