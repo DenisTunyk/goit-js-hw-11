f7fa5a97923fd3362e411ce58304ad3df852333a
DenisTunyk <tundenis43@gmail.com>
2022-12-05 23:08:24 +0200

    fetch

--- src/js/axios.js
+++ src/js/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const BASE_URL = `https://pixabay.com/api`;
-const API_KEY = `31824211-7ef4149e7674ea42a18560458`;
+const API_KEY = `31824211-7ef4149e7674ea42a1856045np8`;
 
 export const fetchPics = async (input, page = "1") => {
   const response = await axios.get(`${BASE_URL}?key=${API_KEY}&q=${input}&orientation=horizontal&safesearch=true&image_type=photo&per_page=40&page=${page}`)
